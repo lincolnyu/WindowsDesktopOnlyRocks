74679739a024db0e9182e0a2fb76e3db804c9374
quanben <quanben@msn.com>
2018-11-14 15:31:07 +1100

    Add cortana as fallback.

--- handywin/Program.cs
+++ handywin/Program.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
 using System.Windows;
 using WindorLib;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace handywin
 {
     class Program
     {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, [Out] StringBuilder lpString, int nMaxCount);
+
         [STAThread]
         static void Main(string[] args)
         {
@@ -21,6 +32,24 @@ namespace handywin
                 {
                     var url = BingMaps.CreateBingMapsUrl(text);
                     Process.Start($"bingmaps:{url}");
+                }
+                else
+                {
+                    // fallback
+                    for (var attempt = 0; attempt < 15; attempt++)
+                    {
+                        var sim = new InputSimulator();
+                        sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_S);
+                        const int nChars = 256;
+                        var p = GetForegroundWindow();
+                        var sb = new StringBuilder(nChars);
+                        if (GetWindowText(p, sb, nChars) > 0 && sb.ToString() == "Cortana")
+                        {
+                            sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
+                            break;
+                        }
+                        Thread.Sleep(200);
+                    }
                 }
             }
         }
