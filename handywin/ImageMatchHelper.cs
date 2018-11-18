c1fc71174d5bd154c064042f1d6afac73b731422
Lincoln Yu <quanben@msn.com>
2018-11-18 22:42:14 +1100

    image matcher to be implemented.

---
+++ handywin/ImageMatchHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace handywin
+{
+    public class ImageMatchHelper
+    {
+        public static bool Search(Bitmap ss, Bitmap target, out Point point, int tolerancePercent = 98)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- handywin/Program.cs
+++ handywin/Program.cs
@@ -1,5 +1,8 @@
+using ScreenShotDemo;
 using System;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
@@ -18,15 +21,86 @@ namespace handywin
         [DllImport("user32.dll")]
         static extern int GetWindowText(IntPtr hWnd, [Out] StringBuilder lpString, int nMaxCount);
 
+        static bool GetForegroundWindowAndTitle(out IntPtr handle, out string title)
+        {
+            const int nChars = 256;
+            handle = GetForegroundWindow();
+            var sb = new StringBuilder(nChars);
+            if (GetWindowText(handle, sb, nChars) > 0)
+            {
+                title = sb.ToString();
+                return true;
+            }
+            title = "";
+            return false;
+        }
+
+        static bool WaitForForeground(string containedTitle, out IntPtr handle,
+            out string actualTitle)
+            => WaitForForeground(containedTitle, TimeSpan.FromSeconds(3), out handle, out actualTitle);
+        
+        static bool WaitForForeground(string title, TimeSpan timeout, out IntPtr handle, 
+            out string actualTitle)
+        {
+            const int intervalMs = 200;
+            var interval = TimeSpan.FromMilliseconds(intervalMs);
+            var start = DateTime.UtcNow;
+            while (true)
+            {
+                if (GetForegroundWindowAndTitle(out handle, out actualTitle) 
+                    && actualTitle.ToLower().Contains(title.ToLower()))
+                {
+                    return true;
+                }
+                if (DateTime.UtcNow - start + interval <= timeout)
+                {
+                    Thread.Sleep(interval);
+                }
+                else
+                {
+                    handle = IntPtr.Zero;
+                    actualTitle = "";
+                    return false;
+                }
+            }
+        }
+
+        static bool WaitForVisualComponentOnScreen(Image vc, out Point point)
+            => WaitForVisualComponentOnScreen(vc, TimeSpan.FromSeconds(10), out point);
+
+        static bool WaitForVisualComponentOnScreen(Image vc, TimeSpan timeout, out Point point)
+        {
+            const int intervalMs = 200;
+            var interval = TimeSpan.FromMilliseconds(intervalMs);
+            var start = DateTime.UtcNow;
+            // https://stackoverflow.com/questions/1163761/capture-screenshot-of-active-window
+            var sc = new ScreenCapture();
+            while (true)
+            {
+                if (sc.CaptureScreen() is Bitmap screenBmp)
+                {
+                    
+                }
+                Thread.Sleep(interval);
+            }
+        }
+
         [STAThread]
         static void Main(string[] args)
         {
             if (Clipboard.ContainsImage())
             {
                 Process.Start("microsoft-edge:http://images.bing.com");
-                // TODO: more advanced stuff
-                // https://stackoverflow.com/questions/1163761/capture-screenshot-of-active-window
-                // ...
+                //Process.Start("iexplore.exe", "http://images.bing.com");
+                if (WaitForForeground("Bing Image Trending", TimeSpan.FromSeconds(10),
+                    out var handle, out var actualTitle))
+                {
+                    var cam = Properties.Resources.camera;
+                    if (WaitForVisualComponentOnScreen(cam, out var point))
+                    {
+
+                    }
+                }
             }
             else if (Clipboard.ContainsText())
             {
@@ -43,19 +117,11 @@ namespace handywin
                 else
                 {
                     // fallback
-                    for (var attempt = 0; attempt < 15; attempt++)
+                    var sim = new InputSimulator();
+                    sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_S);
+                    if (WaitForForeground("Cortana", out var dummy, out var dummy2))
                     {
-                        var sim = new InputSimulator();
-                        sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.VK_S);
-                        const int nChars = 256;
-                        var p = GetForegroundWindow();
-                        var sb = new StringBuilder(nChars);
-                        if (GetWindowText(p, sb, nChars) > 0 && sb.ToString() == "Cortana")
-                        {
-                            sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
-                            break;
-                        }
-                        Thread.Sleep(200);
+                        sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
                     }
                 }
             }
