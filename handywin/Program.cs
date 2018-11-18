81e5d39f7cf41e6175cd1fc6132786d86774137d
lincolnyu <g@b1UtLhb9eN>
2018-11-19 01:27:05 +1100

    cleanup unnecessary code.

--- handywin/Program.cs
+++ handywin/Program.cs
@@ -2,7 +2,6 @@ using ScreenShotDemo;
 using System;
 using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
@@ -21,15 +20,14 @@ namespace handywin
         [DllImport("user32.dll")]
         static extern int GetWindowText(IntPtr hWnd, [Out] StringBuilder lpString, int nMaxCount);
 
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [DllImport("user32.dll")]
         static extern bool SetCursorPos(int x, int y);
 
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [DllImport("user32.dll")]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
         public const int MOUSEEVENTF_LEFTDOWN = 0x02;
         public const int MOUSEEVENTF_LEFTUP = 0x04;
-
 
         static bool GetForegroundWindowAndTitle(out IntPtr handle, out string title)
         {
